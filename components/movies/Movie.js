16b75e9915e72615c561eddb33d72d1aa8635175
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-02 21:54:26 +0100

    tests

--- components/movies/Movie.js
+++ components/movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = ({ id, title }) => {
     <>
       This is movie:
       <Link href={`/movies/${id}`} as={`/movies/${id}`}>
-        {title}
+       1 {title}
       </Link>
     </>
   );
