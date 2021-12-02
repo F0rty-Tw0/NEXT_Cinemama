0e05f20f60d1de1417911018b9f1283e20a113f2
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-02 21:40:49 +0100

    added env to API connection

--- features/authentication/authorizeUser.js
+++ features/authentication/authorizeUser.js
@@ -1,5 +1,5 @@
 import getAuthorization from '../../services/getAuthorization';
-const url = 'https://cine-mama.herokuapp.com/api/auth/login';
+const url = process.env.API_AUTH;
 
 const authorizeUser = async (credentials) => {
   try {
