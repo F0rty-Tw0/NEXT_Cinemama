29b671e3cdce89b89171ddaaca0b14a0025f5ba8
pstepien-design <pstepien@interia.pl>
2021-12-03 22:31:44 +0100

    fixing code

--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,35 @@
+import dayjs from 'dayjs';
 import BaseLayout from 'layouts/BaseLayout';
-import Schedules from '@/components/movies/Movies';
-import { getMovies } from 'endpoints/movies';
-import { getSchedules, getScheduleByDate } from 'endpoints/schedules';
-import { useState, useEffect } from 'react';
+import Schedules from 'components/schedules/Schedules';
+import { getSchedulesBetweenDates } from 'endpoints/schedules';
 
-const Home = ({ movies, schedules }) => {
-  const [day, setDay] = useState('2021-10-23');
-  const today = '2021-10-23';
-  const tomorrow = '2021-10-24';
-  const dayAfterTomorrow = '2021-10-25';
-
-  useEffect(() => {
-    localStorage.setItem('day', day);
-  }, [day]);
-
-  const filteredSchedule = schedules.filter((schedule) => () => {
-    for (let i = 1; i < schedules.length; i++) {
-      if (schedules[i - 1].movie.id == schedules[i].movie.id) {
-        schedules.slice(schedule.movie);
-      }
-    }
-  });
-
-  //for date
-  /*   const [day, setDay] = useState(null);
-  const date = new Date();
-  const today =
-    date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-  const dayTomorrow =
-    date.getFullYear() +
-    '-' +
-    (date.getMonth() + 1) +
-    '-' +
-    (date.getDate() + 1);
-  const dayAfterTomorrow =
-    date.getFullYear() +
-    '-' +
-    (date.getMonth() + 1) +
-    '-' +
-    (date.getDate() + 2);
-
-  useEffect(() => {
-    if (day == null) {
-      setDay(today);
-    }
-  }, [day, today]); */
-
+const Home = ({ schedules, uniqueMovieIds }) => {
   return (
     <BaseLayout
       title='Welcome to the Cinemama Theaters'
       description='The best place to watch movies'
       className='base-layout__main'
     >
-      <p>
-        <button onClick={() => setDay(today)}>Today</button>{' '}
-        <button onClick={() => setDay(tomorrow)}>Tomorrow</button>{' '}
-        <button onClick={() => setDay(dayAfterTomorrow)}>After Tomorrow</button>
-      </p>
-      <Schedules schedule={schedules} day={day} />
+      <Schedules movieIds={uniqueMovieIds} schedules={schedules} />
     </BaseLayout>
   );
 };
-
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
 const getStaticProps = async () => {
-  const movies = await getMovies();
-  const schedules = await getSchedules();
+  const imaginaryDate = dayjs('2021-10-23').format('YYYY-MM-DD');
+  // const today = dayjs().format('YYYY-MM-DD');
+  const schedules = await getSchedulesBetweenDates(
+    imaginaryDate,
+    imaginaryDate
+  );
+  const movieIds = schedules.map((schedule) => schedule.movie.id);
+  const uniqueMovieIds = [...new Set(movieIds)];
   return {
     props: {
-      movies,
       schedules,
+      uniqueMovieIds,
     },
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
