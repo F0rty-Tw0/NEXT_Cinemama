b0b830409aa64c148bab71da7bb06d8429b8c823
Artiom Tofan <artiomtofancv@gmail.com>
2021-12-10 21:15:03 +0100

    added loading on route change

--- components/shared/Loading.js
+++ components/shared/Loading.js
@@ -1,14 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import Image from 'next/image';
 import Modal from 'react-bootstrap/Modal';
 
 const Error = ({ className }) => {
   const { isLoading } = useSelector((state) => state.loading);
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setLoading(true);
+    const handleComplete = (url) => url === router.asPath && setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  });
 
   return (
     <Modal
       className={className}
-      show={isLoading}
+      show={isLoading || loading}
       aria-labelledby='contained-modal-title-vcenter'
       centered
     >
