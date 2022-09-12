a85c90fec9075b1f1ec77e5239a32ffd06998f2e
Art <60610474+F0rty-Tw0@users.noreply.github.com>
2022-09-12 23:20:39 +0300

    Update Footer.js

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -4,7 +4,7 @@ const Footer = ({ className }) => {
       <div className='footer__text'>
         &copy; Designed and developed by:{' '}
         <span className='footer__names'>
-         Artiom Tofan
+         Artiom Tofan, Nikolai Lenander, Pawel Stepien
         </span>
       </div>
     </footer>
